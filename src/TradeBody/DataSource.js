25bdd7d83d2d1a2ef9928e58a88a4586fa307d39
Alfonso Obando <alfonsoloph@gmail.com>
2024-02-28 20:58:22 -0600

    add icon file to the head

--- src/TradeBody/DataSource.js
+++ src/TradeBody/DataSource.js
@@ -4,7 +4,7 @@ export default function DataSource(){
             <h3 >Data Source</h3>
             <p>This project collect hourly data from two the most important 
                 crypto web sides such as <a href="https://www.coinmarketcap.com" target="_blank" rel="noreferrer">
-                Coinmarketcap.com</a> providing of price, volumn, and marketcap about bitcoin, the other 
+                Coinmarketcap.com</a> providing of price, volumn, and marketcap about bitcoin, on the other 
                 hand the second but no less important value that we collect is Fear & Greed Index 
                 from  <a href="https://alternative.me/crypto/fear-and-greed-index/" target="_blank" rel="noreferrer">
                 Alternative.me</a>.<br/>
